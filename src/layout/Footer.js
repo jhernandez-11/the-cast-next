98795db5683c89ace67d03915aa9e9e1f90a7752
jhernandez-11 <jhernandez11@protonmail.com>
2021-01-20 12:31:04 -0800

    final

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -3,13 +3,13 @@ import React from "react";
 const footer = () => {
   return (
     <footer
-      className="flex flex-col justify-around place-items-center h-44 w-full bg-gray-700
+      className="flex flex-col justify-around place-items-center h-44 w-full bg-gray-900
         sm:flex-row sm:h-16"
     >
-      <h3 className="text-lg text-yellow-500">&copy; Jose L Hernandez</h3>
-      <h3 className="text-lg text-yellow-500">About</h3>
-      <h3 className="text-lg text-yellow-500">Contact</h3>
-      <h3 className="text-lg text-yellow-500">More</h3>
+      <h3 className="text-lg text-yellow-300">&copy; Jose L Hernandez</h3>
+      <h3 className="text-lg text-yellow-300">About</h3>
+      <h3 className="text-lg text-yellow-300">Contact</h3>
+      <h3 className="text-lg text-yellow-300">More</h3>
     </footer>
   );
 };
