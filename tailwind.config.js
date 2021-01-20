a09b757690a05d4788d2765f1a2fff5f38f15ffa
jhernandez-11 <jhernandez11@protonmail.com>
2021-01-20 13:10:57 -0800

    tailwind bug

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  purge: {
-    mode: 'all',
-    preserveHtmlElements: false,
-    content: [
-      './src/**/*.js',
-      './node_modules/flatpickr/**/*.js',
-    ],
-  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
